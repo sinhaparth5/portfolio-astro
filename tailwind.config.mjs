68a40b234dcd740f37db2e9e8bb4f113a4a15051
Parth Sinha <sinhaparth555@gmail.com>
2024-12-17 20:33:17 +0000

    added mesh gradient

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'bg-hero': "url('/public/background.svg')",
+				'mesh': "url('/public/test-background.svg')"
+			}
+		},
 	},
 	plugins: [],
 }
