e9ea3fedb623103f0a41456effe4e233c15718f1
Parth Sinha <sinhaparth555@gmail.com>
2024-12-20 18:43:53 +0000

    tailwind updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), svelte()]
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), svelte()]
 });
