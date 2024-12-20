c7442908ad3ed1bcf66ef17d52f5b501478e398b
Parth Sinha <sinhaparth555@gmail.com>
2024-12-20 19:58:29 +0000

    added all the usefull url

---
+++ src/components/svelte/NavBar.svelte
@@ -0,0 +1,14 @@
+<div class="container mx-auto flex border-b-2	border-white pb-2">
+  <div class="flex justify-between items-center w-full flex-col sm:flex-row">
+    <a href="/">
+      <h1 class="dm-sans-bold text-white text-4xl mt-2 bg-mesh">Parth Sinha</h1>
+    </a>
+    <div class="flex justify-between item-centers">
+      <ul class="flex gap-8 text-white text-xl font-bold">
+        <li><a href="/about">About</a></li>
+        <li><a href="/projects">Projects</a></li>
+        <li><a href="/blogs">Blogs</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
