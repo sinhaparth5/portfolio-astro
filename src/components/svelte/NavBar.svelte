be6b78bb5d6ddffd752bc14f26273e91fbef7ed5
Parth Sinha <sinhaparth555@gmail.com>
2024-12-21 08:14:30 +0000

    added dm-sans font

--- src/components/svelte/NavBar.svelte
+++ src/components/svelte/NavBar.svelte
@@ -4,7 +4,7 @@
       <h1 class="dm-sans-bold text-white text-4xl mt-2 bg-mesh">Parth Sinha</h1>
     </a>
     <div class="flex justify-between item-centers">
-      <ul class="flex gap-8 text-white text-xl font-bold">
+      <ul class="flex gap-8 text-white text-xl font-bold dm-sans-bold">
         <li><a href="/about">About</a></li>
         <li><a href="/projects">Projects</a></li>
         <li><a href="/blogs">Blogs</a></li>
