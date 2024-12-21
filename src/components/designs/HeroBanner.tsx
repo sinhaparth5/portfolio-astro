7cad8966d5da62d48ff2c42f7ff52dabe575fa51
Parth Sinha <sinhaparth555@gmail.com>
2024-12-21 09:53:25 +0000

    added hero banner

--- src/components/designs/HeroBanner.tsx
+++ src/components/designs/HeroBanner.tsx
@@ -1,23 +1,27 @@
-"use client";
 import React from "react";
-import { motion } from "framer-motion";
-import { LampContainer } from "../ui/lamp";
+import { Vortex } from "../ui/vortex";
 
 export function HeroBanner() {
   return (
-    <LampContainer>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+    <div className="w-[calc(100%-4rem)] mx-auto rounded-md  h-[30rem] overflow-hidden">
+      <Vortex
+        backgroundColor="black"
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
       >
-        Build lamps <br /> the right way
-      </motion.h1>
-    </LampContainer>
+        <h2 className="text-white text-2xl md:text-6xl font-bold text-center dm-sans-bold">
+          The hell is this?
+        </h2>
+        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center dm-sans-normal">
+          This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
+          burned and you&apos;ll have a scar.
+        </p>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset] dm-sans-normal">
+            Order now
+          </button>
+          <button className="px-4 py-2  text-white dm-sans-normal">Watch trailer</button>
+        </div>
+      </Vortex>
+    </div>
   );
 }
